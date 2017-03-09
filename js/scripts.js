21506bca1b2b0a82e71c1e0453f94d197b11162d
Abdelaziz Khabthani <abdelaziz.khabthani@gmail.com>
2017-03-09 15:37:59 +0100

    more delay

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $("html").hide();
 $(function() {
-    $("html").hide().delay(500).fadeIn(2200);
+    $("html").hide().delay(1000).fadeIn(2200);
     $('.container').css({
         'max-width': window.screen.width / 3
     });
